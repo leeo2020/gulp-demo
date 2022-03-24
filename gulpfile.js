154c4d4907c493a893bcf713f55acf8845bbeebf
leeo2020 <3145875098@qq.com>
2022-03-25 00:43:28 +0800

    xiugai-gulp-html

--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,23 @@ const del = require('del')
 const server = require('gulp-webserver')
 
 const cssHandle = () => {
-	return gulp.src('./src/css/*.css')
+	return gulp
+		.src('./src/css/*.css')
 		.pipe(prefixer())
 		.pipe(cssmin())
 		.pipe(gulp.dest('./dist/css'))
 }
 const sassHandle = () => {
-	return gulp.src('./src/scss/*.scss')
+	return gulp
+		.src('./src/scss/*.scss')
 		.pipe(sass())
 		.pipe(prefixer())
 		.pipe(cssmin())
 		.pipe(gulp.dest('./dist/css'))
 }
 const lessHandle = () => {
-	return gulp.src('./src/less/*.less')
+	return gulp
+		.src('./src/less/*.less')
 		.pipe(less())
 		.pipe(prefixer())
 		.pipe(cssmin())
@@ -33,46 +36,51 @@ const lessHandle = () => {
 }
 
 const htmlHandle = () => {
-	return gulp.src('./public/*.html')
-		.pipe(htmlmin({
-				'collapseWhitespace': true,
-				'removeAttributeQuotes': true,
-				'collapseBooleanAttributes': true,
-				'removeComments': true,
-				'minifyCss': true,
-				'minifyJs': true
-			})
-				.pipe(gulp.dest('./dist/public'))
-		)
+	return (
+		gulp
+			.src('./public/*.html')
+			// 压缩html需要配置压缩的参数
+			.pipe(
+				htmlmin({
+					collapseWhitespace: true, //压缩空格
+					removeAttributeQuotes: true, //移除属性的引号
+					collapseBooleanAttributes: true, //把值为布尔值的属性简写
+					removeComments: true, //移除注释
+					minifyCSS: true, //把页面里面的style标签里面的css样式也去空格
+					minifyJS: true, //把页面里的script标签里面的js代码给去空格
+				})
+			)
+			.pipe(gulp.dest('./dist/pages'))
+	)
 }
 
 const jsHandle = () => {
-	return gulp.src('./src/js/*.js')
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
+	return gulp
+		.src('./src/js/*.js')
+		.pipe(
+			babel({
+				presets: ['@babel/env'],
+			})
+		)
 		.pipe(uglify())
 		.pipe(gulp.dest('./dist/js'))
 }
 
 const libHandle = () => {
-	return gulp.src('./src/lib/**')
-		.pipe(gulp.dest('./dist/lib'))
+	return gulp.src('./src/lib/**').pipe(gulp.dest('./dist/lib'))
 }
 
 const imgHandle = () => {
-	return gulp.src('./src/img/*.{png,jpg,gif,ico}')
-		.pipe(gulp.dest('./dist/img'))
+	return gulp.src('./src/img/*.{png,jpg,gif,ico}').pipe(gulp.dest('./dist/img'))
 }
 
 const fontsHandle = () => {
-	return gulp.src('./fonts/**')
-		.pipe(gulp.dest('./dist/fonts'))
+	return gulp.src('./fonts/**').pipe(gulp.dest('./dist/fonts'))
 }
 
 const delHandle = () => {
 	if (fs.existsSync(path.dirname(path.resolve(__dirname, './dist')))) {
-		return del('./dist');
+		return del('./dist')
 	}
 }
 
@@ -88,15 +96,22 @@ const watchHandle = () => {
 }
 
 const webHandle = () => {
-	return gulp.src('./dist')
-		.pipe(server({
+	return gulp.src('./dist').pipe(
+		server({
 			port: 9999,
-			open: '/public/index.html',
-			livereload: true
-		}))
+			open: '/pages/index.html',
+			livereload: true,
+			proxies: [
+				{
+					source: '/weather',
+					target: 'https://way.jd.com/jisuapi/weather',
+				},
+			],
+		})
+	)
 }
 
-module.exports.default = gulp.series(
+exports.default = gulp.series(
 	delHandle,
 	gulp.parallel(
 		htmlHandle,
